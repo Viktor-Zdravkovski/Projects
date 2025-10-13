cb29e6c3629085488edb681be7703b23b83d0383
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2025-10-13 23:20:14 +0200

    designing frontend

---
+++ C#/Hotel Management/HotelManagementSystem/myhotelmanagementsystemapp/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header/Header.jsx'
+import Hero from './Hero/Hero.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default App
