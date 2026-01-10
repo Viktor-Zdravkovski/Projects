2d4fc37a4c0cbc435d0480a330d850352885f57d
unknown <viktorzdravkovski5@gmail.com>
2026-01-10 20:30:12 +0100

    finished code

---
+++ C#/Hotel Management/HotelManagementApp/HotelManagement.DataBase/Migrations/20260110191626_Adjusted_Names.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelManagement.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adjusted_Names : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Method",
+                table: "Payments",
+                newName: "PaymentMethod");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentMethod",
+                table: "Payments",
+                newName: "Method");
+        }
+    }
+}
